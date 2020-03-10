e35aae2d293295b95f3e36a1ea409599d77a4a21
Ricardy99 <ricardyc@gmail.com>
2020-03-10 16:20:31 -0400

    updated

---
+++ rfid.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>     
+    </head>
+<body>
+
+<h1>My First Heading</h1>
+
+<p>My first paragraph.</p>
+
+
+<!--
+    document.querySelector('button') 
+  .addEventListener(() => fetch('runMyScript.php', { method: 'POST' }));
+-->
+
+<?php
+ //   <p2>Now, for more options, click <a href=options.php>on this</a> and good luck!</p2>
+   // system('python3 /home/pi/pi-rfid/test3.py');
+?>
+
+<input type="button" name="open" value="open" onclick=exec(python3 /home/pi/pi-rfid/test3.py)>
+
+</body>
+</html>
