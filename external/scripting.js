2d75f68b79d04491e62d0ee353e596beb12bfd0d
Ricardy99 <ricardyc@gmail.com>
2020-04-24 14:05:22 -0400

    Step 1
    Added menu bar in home page, with scripting and styling files.

---
+++ external/scripting.js
@@ -0,0 +1,13 @@
+/*
+function toggleSidebar(){
+    document.getElementById("sidebar").classList.toggle("active");
+}
+*/
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
